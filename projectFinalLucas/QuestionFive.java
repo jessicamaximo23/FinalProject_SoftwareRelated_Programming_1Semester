3476cd86d4577d006742dd4555d7ab727695974e
Jessica Maximo <jessicamaximo23.com>
2023-11-01 13:07:31 -0400

    Update Question5

--- projectFinalLucas/QuestionFive.java
+++ projectFinalLucas/QuestionFive.java
@@ -9,28 +9,26 @@ public class QuestionFive {
 		// Assignment Final Project
 		// Written by:Jessica Maximo De Souza ID: 2391133
 		// Question Five:Logical Flowchar
-		//This question I tried to divide the sides per segment because I can only use three variables
+		// In this question I tried to use array and divide the sides by segments
+		// but in the end I could only reach the result using 4 variables instead of using 3
 		// -----------------------------------------------------
 		Scanner kb = new Scanner(System.in);
 
+		//Initialize variable side	
 		int side = 7;
 
-		// Create array for keep the sizes of each segment
-		double[] sides = new double[side];
-
-		// User put all the number of the segment
+		// User put numbers for each side
 		for (int i = 0; i < side; i++) {
-			System.out.println("Lenght of the segment (cm) " + (i + 1) + "?");
+			System.out.println("Lenght of the segment " + (i + 1) + "?");
 			
-			// array to keep this sides
-			sides[i] = kb.nextDouble();
+			// Which side
+			double length = kb.nextDouble();
 
 			// Perimeter = sum all sides
 			double sumPerimeter = 0;
-			for (double length : sides) {
 				sumPerimeter += length;
-			}
-			System.out.println("Total Perimeter: " + sumPerimeter);
+			
+			System.out.println("Total Perimeter: " + sumPerimeter + " cm");
 		}
 	}
 }
